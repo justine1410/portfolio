ce5c64e6189d34210f9c9f830376492702b43d2f
justine <justine1410@live.fr>
2022-03-31 13:35:37 +0200

    commencement du portfolio

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+$("footer").html(`
+        <ul>
+            <li>
+                <a href="mention.html"> Mentions légale</a>
+            </li>
+            <li>
+                <p>Fidweb <br> 2021 </p> 
+            </li>
+            <div class="reseaux">
+                <li>
+                    <a href="https://www.facebook.com/Fidweb-101859772243699"  aria-label="facebook" target="_blank" rel="noopener noreferrer" class="hover" >
+                        <i class="fab fa-facebook " ></i>
+                    </a>    
+                </li>
+                <li>
+                    <a href="https://github.com/justine1410" aria-label="twitter" target="_blank" rel="noopener noreferrer" class="hover">
+                    <i class="fab fa-github "></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/justinefidel/" aria-label="twitter" target="_blank" rel="noopener noreferrer" class="hover">
+                        <img src="./img/icons8-linkedin.gif" alt="">
+                    </a>
+                </li>
+
+            </div>
+        </ul>
+`)
---
+++ js/header.js
@@ -0,0 +1,26 @@
+$("header").html(`
+    <nav>
+        <label for="toggle">☰</label>
+        <input type="checkbox" id="toggle">
+        <div class="main_pages">
+            <div class="logo">
+                <img src="./img/logo1.png" alt="">
+            </div>
+            <a href="#presentation" class="hover" class="active">
+                Qui suis-je ?
+            </a>
+            <a href="#projet" class="hover">
+                Projet de formation 
+            </a>
+            <a href="#contact" class="hover">
+                Contact
+            </a>
+                
+        </div>
+    </nav>
+
+`)
+
+
+
+
