647d091c140efce10086ee2b8bd4b1e1699f7dc1
justine <justine1410@live.fr>
2022-04-19 21:16:31 +0200

    style

--- js/footer.js
+++ js/footer.js
@@ -3,29 +3,22 @@ $("footer").html(`
             <li>
                 <a href="mention.html"> Mentions légale</a>
             </li>
-            <li>
-                <p>Fidweb <br> 2021 </p> 
-            </li>
+            
             <div class="reseaux">
                 <li>
                     <a href="https://www.facebook.com/Fidweb-101859772243699"  aria-label="facebook" target="_blank" rel="noopener noreferrer" class="hover" >
-                        <i class="fab fa-facebook " ></i>
+                        <i class="fab fa-facebook fa-xl " ></i>
                     </a>    
                 </li>
                 <li>
                     <a href="https://github.com/justine1410" aria-label="twitter" target="_blank" rel="noopener noreferrer" class="hover">
-                    <i class="fab fa-github "></i>
+                    <i class="fab fa-github fa-xl"></i>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/justinefidel/" aria-label="twitter" target="_blank" rel="noopener noreferrer" class="hover">
                     <img src="./img/icons8-linkedin-2 (5).gif" alt="">
                     </a>
-                </li>
-                <li>
-                    <p>
-                    <a target="_blank" href="https://icons8.com/icon/20909/html-5"></a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-                    </p>
                 </li>
                 </li>
 
